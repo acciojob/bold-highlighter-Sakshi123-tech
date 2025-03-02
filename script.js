35f4f96fccd6fa5ccc5ee8b9557d4a845a6a9caf
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-03-03 00:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let strongText = document.querySelectorAll("strong");
+
 function highlight() {
-    //Write your code here
-
-
+    // Write your code here
+    strongText.forEach(t => {
+        t.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    // Write your code here
+    strongText.forEach(t => {
+        t.style.color = "rgb(0, 0, 0)";
+    });
 }
